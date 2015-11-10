221892d68bb506395fe5f57ef01aacf8d21ba4e0
MoisesMontano <moisesdanielmontano@gmail.com>
2015-11-09 22:09:41 -0300

    Paquetes
    Titulo de la Modal

--- include/header.php
+++ include/header.php
@@ -43,7 +43,7 @@
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Modal title</h4>
+						<h4 class="modal-title">Paquetes Personalizados</h4>
 					</div>
 					<div class="modal-body">						
 						Tipo de Paquete:<select name="cmbPaquete" id="cmbPaquete" onchange="Paquete();">							
